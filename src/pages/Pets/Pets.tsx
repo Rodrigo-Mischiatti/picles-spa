839654ec81297ba266f21bb44d505a5f83edcb29
Rodrigo-Mischiatti <rodrigomisc@hotmail.com>
2024-04-17 22:25:31 -0300

    Criado o componente Card, Importamos o Axion para tratar das requisições REST, tb importamos o Skeleton para mostrar o componente de loading na tela. ajustamos o .Env com o caminho do banco. criamos tg a função para tratar a imagem base64

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -2,12 +2,24 @@ import { Link } from "react-router-dom";
 import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid";
 import styles from "./Pets.module.css"
+import { Card } from "../../components/common/Card";
+import { Skeleton } from "../../components/common/Skeleton";
+import { getPets } from "../../services/pets/getPets";
+
 
 export function Pets() {
     return (
         <Grid>
             <div className={styles.container}>
                 <Header />
+                <main className={styles.list}>
+                    <Skeleton count={5} containerClassName={styles.skeleton} />
+
+
+                    <Card href="/pets/1" text="nina" thumb="" />
+                    <Card href="/pets/2" text="bob" thumb="" />
+                    <Card href="/pets/3" text="chico" thumb="" />
+                </main>
                 <Link to="/pets/20">Ir para Listagem</Link>
             </div>
         </Grid>
