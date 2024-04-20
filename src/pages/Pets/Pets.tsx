be3eeadb2a2a23e8b9bfd17a030bfffe2027d4b4
Rodrigo-Mischiatti <rodrigomisc@hotmail.com>
2024-04-19 22:27:00 -0300

    Criamos o componente Select, uma constante padronizada para utilizar no Select e ajustamos a pagina de Pets para utilizar o componente Select criado para gerar os filtros para enviar para backend com os parametros na URL. Tb foi feito tratativa para a pagina não fazer reload a cada filtro, apenas faz a chamada da API e atualiza a pagina sem reload.

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -6,7 +6,11 @@ import { Skeleton } from "../../components/common/Skeleton";
 import { Pagination } from "../../components/common/Pagination";
 import { useSearchParams } from "react-router-dom";
 import { usePetList } from "../../hooks/usePetList";
-
+import { Select } from "../../components/common/Select";
+import { Button } from "../../components/common/Button";
+import { filterColumns } from "./Pets.constants";
+import { FormEvent } from "react";
+import { GetPetsRequest } from '../../interfaces/pet'
 
 export function Pets() {
 
@@ -14,6 +18,9 @@ export function Pets() {
 
     const urlParams = {
         page: searchParams.get('page') ? Number(searchParams.get('page')) : 1,
+        type: searchParams.get('type') ?? '',  //se nao tiver especificado o parmetro type, queremos que na URL fique uma string vazia e não undefined
+        size: searchParams.get('size') ?? '',
+        gender: searchParams.get('gender') ?? ''
     }
 
     const { data, isLoading } = usePetList(urlParams)
@@ -25,10 +32,58 @@ export function Pets() {
         })
     }
 
+
+    function getFormValue(form: HTMLFormElement) {
+        const formData = new FormData(form)
+        return Object.fromEntries(formData)
+    }
+
+    function updateSearchParams(urlParams: GetPetsRequest) {
+        const fields: (keyof GetPetsRequest)[] = ['type', 'size', 'gender']
+        const newParams = new URLSearchParams()
+
+        fields.forEach((field) => {
+            if (urlParams[field]) {
+                newParams.set(field, String(urlParams[field]))
+            }
+        })
+        newParams.set('page', '1')
+
+        return newParams
+    }
+
+    function applyFilters(event: FormEvent) {
+        event.preventDefault()                       //começa previnindo para não fazer reload de pagina
+
+        const formValues = getFormValue(event.target as HTMLFormElement)
+        const newSearchParams = updateSearchParams(formValues)
+
+        setSearchParams(newSearchParams)
+    }
+
+
     return (
         <Grid>
             <div className={styles.container}>
                 <Header />
+                <form className={styles.filters} onSubmit={applyFilters}>
+                    <div className={styles.columns}>
+                        {
+                            filterColumns.map((filter) => (
+                                <div key={filter.name} className={styles.column}>
+                                    <Select
+                                        label={filter.name}
+                                        defaultValue={urlParams[filter.name]}
+                                        name={filter.name}
+                                        options={filter.options}
+                                    />
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <Button type="submit">Buscar</Button>
+                </form>
+
                 {
                     isLoading && (
                         <Skeleton
